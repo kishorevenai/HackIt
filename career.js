466009b0b7d8e2882facd1a6d92097412c9998c5
kishorevenai <kishorevenai@gmail.com>
2023-06-09 14:03:08 +0530

    commited june 9

--- career.js
+++ career.js
@@ -1,14 +1,46 @@
 // const moon = document.querySelector('.darkmode')
 // // const sun = document.querySelector('.darkmode')
 
+const ShowDesc = document.querySelectorAll('.btn_2')
+const Description = document.querySelectorAll('.Description')
+console.log(Description)
+
+// ShowDesc = document.addEventListener('click',()=> {
+
+// })
 
 
-// moon.addEventListener('click', () =>{
+ShowDesc.forEach(item => item.addEventListener('click',()=> {   
+   Description.forEach(items => {
+      if(item.id === items.id) {
+         if(item.getAttribute("data-status") === "true") {
+            items.style.height = "100px"
+            item.setAttribute("data-status","false")      
+         }
+         else {
+            items.style.height = "0px"
+            item.setAttribute("data-status","true")            
+         }
+      }
+   })
+}))
 
-//    moon.classList.toggle('active')
-   
-// } )
-// console.log(moon)
+
+// --------------------Slider---------------------------
+
+
+const leftArrow = document.querySelector('.leftArrow')
+const rightArrow = document.querySelector('.rightArrow')
+const slider_arrow = document.querySelector('.slider_arrow')
+
+leftArrow.addEventListener('click', () => {
+   slider_arrow.style.transform = "translateX(-200px)"
+})
+rightArrow.addEventListener('click', () => {
+   slider_arrow.style.transform = "translateX(200px)"
+})
+
+// ---------------------------Dark mode------------------------------------
 
 function changeMode() {
    const body = document.body;
